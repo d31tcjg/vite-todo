88695781559863cd93edd3a14c1a41ceb457353f
Cesar Garcia <d31tcjg@gmail.com>
2021-02-10 15:21:07 -0600

    Scafold all the things!

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-6xl font-thin text-green-700 text-center">{{ msg }}</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  msg: {
+    type: String,
+    default: 'Custom Title',
+  },
+})
+</script>
